ac99621e5aff1f0a2615912015c731c579982146
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-02 19:32:26 +0200

    Update processillumina.py
    - added function `parseDoubleFastaq(filename, verbose=True)`
      - return a dictionary  in format {Header:[sequence,scoreascii]}for a scuffed copy paste of an xlsx workbook tab into notepad
    - added function `checkProperFormat` (placeholder)
    - moved most of `parseFastaQ` to `parseGoodFastaQ`
      - handles FastaQ in previous format where header, seq, avscore, asciiscore each had their own line
    - changed `parseFastaQ`
      - throw error if `filenames` is not of type list
      - check each file for scuffed-ness ( still assuming files exist ) and handle accordingly
    - changed main to test this new thing

--- biopython_script/processillumina.py
+++ biopython_script/processillumina.py
@@ -1,7 +1,9 @@
 
 FILE_1 = "-at-HWI-M02942_file1.txt"
 FILE_2 = "-at-HWI-M02942_file2.txt"
+
 FILES = [FILE_1, FILE_2]             # automate finding files later
+
 
 COMMON_IDENTIFIERS = [
 "@HWI-M02942:21:000000000-ACNW4:1:1101:11964:3049",
@@ -10,34 +12,111 @@ COMMON_IDENTIFIERS = [
 "@HWI-M02942:21:000000000-ACNW4:1:1101:8317:2760"
 ]
 
+TESTFILE_1 = "testdata_file1.txt"
+SCUFFED_XLSX_DATA_SET_FILE = "data_set1.txt"
+
+
+
+class ScuffedDataError(BaseException):
+    pass
+
+
+
 # we use common identifiers ( identifiers to find fastaQ entry pairs
   # from file 1 and 2 ) for testing purposes/weektaak1
 def main():
     print("RUNNING MAIN (FOR TESTING! PROCESSILUMINA V.1")
 
     # create test files
-    data = parseFastaQ(FILES,COMMON_IDENTIFIERS)
-    print(data)
-            
-    print("DONE")
+    #data = parseFastaQ(FILES,COMMON_IDENTIFIERS)
+    #print(data)
+    parseFastaQ([SCUFFED_XLSX_DATA_SET_FILE ,TESTFILE_1], verbose=True)       
+        
+    print("TESTING DONE")
 
-def parseFastaQ(filenames, common_ids,verbose=False):
+
+def parseDoubleFastaQ(filename, verbose=True):
+    """ return a dictionary  in format {Header:[sequence,scoreascii]}
+        for a scuffed copy paste of an xlsx workbook tab into notepad"""
+
+    data = dict()
+
+    with open (filename, 'r') as f:
+
+        for line in f:
+
+            [h1, s1, asc1, h2, s2, asc2] = line.split("\t")
+
+            data[h1] = [s1,'noavscore',asc1]
+            data[h2] = [s2,'noavscore',asc2[:-2]] # :-2 to remove trailing newline
+
+            if verbose:
+                # tested on data_set1, 
+                print("H1",h1.split(":")[-1],"allDNA=",all([s in "ATCG" for s in s1]),
+                        "H2",h2.split(":")[-1],"allDNA=",all([s in "ATCG" for s in s2]))
+    return data
+
+
+def checkProperFormat(filename):
+    """placeholder function
+    - test for a pattern in headers
+    - test for conistency in data e.g. lines presented in
+    a, b, c, d -> repeat without missing parts
+    - test for all([s in "ATCG" for s in seqeunce]
+    """
+    pass
+
+def testForScuffed(filename):
+    """ returns True if a file is tab delimited with both reads on one line
+        e.g.
+        @Header1\tSeq1\tasciScore1\t@Header2\tSeq2\tasciScore2"""
+
+    with open (filename, 'r') as f:
+        line = f.readline()
+        if len(line.split("\t")) > 2:
+            print("you got scuffed data")
+            return True
+        else:
+            print("That data could be proper fastaQ format")
+            return False
+
+
+
+
+def parseGoodFastaQ(filename, common_ids=[],verbose=False):
+
+    data = dict()
+    with open (filename, 'r') as f:
+        for line in f:
+            for identifier in common_ids:
+                if (identifier in line) and (
+                        "@HWI" in line):
+                    identifier = line
+                    data[identifier] = []
+                    for i in range(3):
+                        data[identifier].append(f.readline().
+                                                replace("\n", ""))
+    return data
+
+def parseFastaQ(filenames, common_ids=[],verbose=False):
     """ assume string 'HWI' in every header"""
+
+    # filenames should be a list, otherwise, throw error
+    if type(filenames) != list:
+        raise ValueError("I know it's awkward, but make sure the filename(s) are in a list")
     
     data = dict()
 
     for filename in filenames:
-        with open (filename, 'r') as f:
 
-            for line in f:
-                for identifier in common_ids:
-                    if (identifier in line) and (
-                            "@HWI" in line):
-                        identifier = line
-                        data[identifier] = []
-                        for i in range(3):
-                            data[identifier].append(f.readline().
-                                                    replace("\n", ""))
+        # test for scuffed and handle accordingly
+        if testForScuffed(filename):
+            part_data = parseDoubleFastaQ(filename)
+        else:
+            part_data = parseGoodFastaQ(filename)
+            
+        # merge dicts
+        data = {**data, **part_data}
 
 
     if verbose:
@@ -45,6 +124,7 @@ def parseFastaQ(filenames, common_ids,verbose=False):
             print(k, v)
 
     print("parsed fasta files!")
+    
     return data
     print(10*"-")
 

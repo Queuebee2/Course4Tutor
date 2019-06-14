59365f779f8a5d0e212dd5ab909123ed46b2914c
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-14 23:52:51 +0200

    speed rehaul lets go

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ import Bio.Blast.Record as record
 from biopython_script.DbConnector import DbConnector
 from flask import Flask, request, render_template, url_for, Markup
 from Bio.Seq import Seq
+import os
 
 app = Flask(__name__)
 
@@ -24,10 +25,10 @@ def forms_to_dict():
         if request.form['button'] == 'submit_forms':
             form_results = request.form.to_dict()
             print(form_results)
-            return render_template('forms_to_dict_test.html', formables=formables, form_results=form_results)
+            return render_template('forms.html', formables=formables, form_results=form_results)
 
     elif request.method == 'GET':
-        return render_template('forms_to_dict_test.html',formables=formables)
+        return render_template('forms.html',formables=formables)
 
 
 
@@ -92,7 +93,7 @@ def blast():
     return render_template('zelf_blasten.html')
 
 
-@app.route('/selectdemo', methods=["GET","POST"])
+@app.route('/resultaten', methods=["GET","POST"])
 def select_results_demo():
 
     if request.method == 'POST':
@@ -100,11 +101,11 @@ def select_results_demo():
             connect = DbConnector()
             results = connect.select_results()
             results_html_table = Markup(createHtmlTable(results))
-            return render_template('resultaten_demo.html', blast_results=results_html_table)
+            return render_template('resultaten.html', blast_results=results_html_table)
         else:
-            return render_template('resultaten_demo.html')
+            return render_template('resultaten.html')
     else:
-        return render_template('resultaten_demo.html')
+        return render_template('resultaten.html')
 
 def createHtmlTable(rows):
     ts = "<div id = 'complete_tabel'>"
@@ -128,6 +129,30 @@ def createHtmlTable(rows):
     ts += "</div>"
     ts += "</div>"
     return ts
+
+# lijst van templates die we niet willen in de menubalk
+removables = ['__nav_bar','readme.md','__footer','__layout','__header']
+
+def navigationMenu():
+    # de template namen worden opgehaald en geparsed en in een lijst gezet
+    # en available gemaakt als global
+    NAVIGATION_ITEMS = [item.replace(".html", '') for item in os.listdir('templates')]
+
+    # removable items worden weggehaald
+    for item in removables:
+        try:
+            popindex = NAVIGATION_ITEMS.index(item)
+            NAVIGATION_ITEMS.pop(popindex)
+        except ValueError:
+            pass
+
+    # return items voor navigatiemenu
+    return NAVIGATION_ITEMS
+
+
+@app.context_processor # injecteer navigatie items als default in render_template
+def inject_navbar():
+    return dict(NAVIGATION_ITEMS=navigationMenu())
 
 
 if __name__ == '__main__':

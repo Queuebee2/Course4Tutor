62d30d591370e37617dd7ae009325f90bb46094e
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-14 23:02:38 +0200

    Update insert_script.py

--- biopython_script/insert_script.py
+++ biopython_script/insert_script.py
@@ -1,4 +1,3 @@
-from db_insert import DbConnector
 from DbConnector import DbConnector
 
 DATA_FILE = "new_tab_delimited_stuff.txt"
@@ -7,19 +6,81 @@ def main():
 
 
     connector = DbConnector()
+    print('hh')
+    datadict =  {
+                'accessiecode': "testcustom",
+                'e_value':9e-150,
+                'setting_id':1,
+                'percentage_identity':11,
+                'total_coverage':12,
+                'max_scores':13,
+                'query_coverage':14
+                }
+    connector.insert_from_form_data(datadict)
+
+    searchdict = {'resultaat_id':251}
+    searchdict2 = {'resultaat_id':251,
+                   'resultaat_id':241}
+    searchdict3 = {'accessiecode': "testcustom"}
+                    
+    list_many(connector, [searchdict,
+    searchdict2,
+    searchdict3])
+
+def list_many(c, dicts):
+    for d in dicts:
+        listResults(c, d)
+
+    # this is so bad...
+def listResults(connector, queryDict):
+    results = connector.search_from_form_data(queryDict)
+
+    for i in results:
+        print(i)
+    """
     with open (DATA_FILE, 'r') as f:
 
         lines = list(set(f.readlines()))
 
         for line in lines:
-            if 'title' in line and 'score' in line and 'matrix' in line:
+            if isHeaderLine(line):
                 pass
             else:
-
                 query, values = genQuery(line.split("\t"), True)
                 connector.insert(query, values)
+    """
+    
+    def query_for_formData(formdata):
 
 
+        values = []
+        query_start  = "insert into blast_result("
+        query_tables = ""
+        
+        for k, v in formdata.items():
+            query_tables += '`'+str(k)+'`,'
+            values.append(v)
+
+        query_tables = query_tables[:-1] # strip ,
+
+        query_end = ")"
+        
+        query = query_start + query_tables + query_end
+        
+        return query, values
+
+
+def isHeaderLine(line):
+    """" assume a string/list contains only headers when these keywords exist
+    """
+    has_title = 'title' in line
+    has_score = 'score' in line
+    has_matrix = 'matrix' in line
+
+    if has_title and has_score and has_matrix:
+        return True
+    else:
+        return False
 
 """Queries (for now) are specific to
 insert into settings(id, `database`, alg_naam, scorematrix)
